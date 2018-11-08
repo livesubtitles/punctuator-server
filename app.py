1a783fa0f9d13b5542f6ebf7ef75891cd8eb5ace
eleni <elenialevra98@gmail.com>
2018-11-08 19:37:15 +0000

    Only create model file if it does not exist

--- app.py
+++ app.py
@@ -10,10 +10,26 @@ CORS(app, resources={r"/punctuate": {"origins": "*"}})
 
 first_request = True
 
+def prepare_model_file():
+    f1 = open("./punctuator2/model1.pcl", "rb")
+    final_file = open("./punctuator2/final-model.pcl", "wb")
+    for line in f1:
+        final_file.write(line)
+    f1.close()
+    print("Writing first file completed")
+    f2 = open("./punctuator2/model2.pcl", "rb")
+    for line in f2:
+        final_file.write(line)
+    f2.close()
+    print("Writing second file completed")
+    final_file.close()
+
 @app.route("/punctuate", methods=['POST'])
 def punctuate():
     global first_request
     if (first_request):
+        if (not os.path.isfile("./punctuator2/final-model.pcl")):
+            prepare_model_file()
         model_path = "./punctuator2/final-model.pcl"
         init_punctuator(model_path)
         first_request = False
